2af07c437f31a488ae78adcbb36e033e1be1060f
ULookup <chbulookup@outlook.com>
2026-06-19 08:05:05 +0000

    feat: add workspace file rename/move endpoint

--- libs/http/include/merak/http_server.hpp
+++ libs/http/include/merak/http_server.hpp
@@ -63,6 +63,7 @@ private:
     void handle_workspace_file_content_put(const httplib::Request& req, httplib::Response& res);
     void handle_workspace_file_create(const httplib::Request&, httplib::Response&);
     void handle_workspace_file_delete(const httplib::Request&, httplib::Response&);
+    void handle_workspace_file_rename(const httplib::Request&, httplib::Response&);
 
     std::shared_ptr<LlmProvider> llm_provider_;
     nlohmann::json cached_config_;
